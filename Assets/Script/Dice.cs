68852362a9722f9e2382fd79ebffc693e5372448
sojebsikder <sojebsikder@gmail.com>
2021-02-20 16:32:18 +0600

    main

---
+++ Assets/Script/Dice.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour {
+
+    public GameObject Dice1;
+    public GameObject Dice2;
+
+
+    private Sprite[] diceSides;
+    private SpriteRenderer rend1;
+    private SpriteRenderer rend2;
+    private int whosTurn = 1;
+    private bool coroutineAllowed = true;
+
+
+	// Use this for initialization
+	private void Start () {
+        rend1 = Dice1.GetComponent<SpriteRenderer>();
+        rend2 = Dice2.GetComponent<SpriteRenderer>();
+
+        diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+        rend1.sprite = diceSides[0];
+        rend2.sprite = diceSides[5];
+	}
+
+    public void RollDice()
+    {
+        if (!GameManager.gameOver && coroutineAllowed)
+            StartCoroutine("RollTheDice");
+    }
+
+    /*
+    private void OnMouseDown()
+    {
+        if (!GameControl.gameOver && coroutineAllowed)
+            StartCoroutine("RollTheDice");
+    }
+     * */
+
+    private IEnumerator RollTheDice()
+    {
+        gameObject.GetComponent<Button>().enabled = false;
+
+        coroutineAllowed = false;
+        int randomDiceSide1 = 0;
+        int randomDiceSide2 = 0;
+        for (int i = 0; i <= 20; i++)
+        {
+            randomDiceSide1 = Random.Range(0, 6);
+            randomDiceSide2 = Random.Range(0, 6);
+            rend1.sprite = diceSides[randomDiceSide1];
+            rend2.sprite = diceSides[randomDiceSide2];
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        // Set dice step
+        GameManager.diceSideThrown = (randomDiceSide1 + 1) + (randomDiceSide2 + 1);
+
+
+        if (whosTurn == 1)
+        {
+            GameManager.MovePlayer(1);
+        } else if (whosTurn == -1)
+        {
+            GameManager.MovePlayer(2);
+        }
+        whosTurn *= -1;
+        coroutineAllowed = true;
+
+        gameObject.GetComponent<Button>().enabled = true;
+    }
+}
---
+++ Assets/Script/SaveSystem.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+public static class SaveSystem
+{
+    public static void SaveGame(GameManager gameManager)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/gameData.bin";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData data = new GameData(gameManager);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static GameData LoadGame()
+    {
+        string path = Application.persistentDataPath + "/gameData.bin";
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+           GameData data = formatter.Deserialize(stream) as GameData;
+           stream.Close();
+
+           return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+    public static void ClearGame()
+    {
+        string path = Application.persistentDataPath + "/gameData.bin";
+        File.Delete(path);
+          
+    }
+}
